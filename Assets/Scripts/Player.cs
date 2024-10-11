d0faf85e3a2b0196f07da3c9ecfdaea46b812b21
refigo <refi@xyzcorp.io>
2024-10-11 15:09:32 +0900

    Feat: Animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
+    private bool isWalking;
 
     private void Update() {
         Vector2 inputVector = new Vector2(0, 0);
@@ -26,6 +27,14 @@ public class Player : MonoBehaviour
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
-        Debug.Log(Time.deltaTime);
+
+        isWalking = moveDir != Vector3.zero;
+
+        float rotateSpeed = 50f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+
+    public bool IsWalking() {
+        return isWalking;
     }
 }
