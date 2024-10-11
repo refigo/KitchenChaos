6d0138de0e2e6ed9b024626d7337ffa81fbbe370
refigo <refi@xyzcorp.io>
2024-10-11 11:28:30 +0900

    Feat: Set player moveSpeed SerializeField private

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 7f;
 
     private void Update() {
         Vector2 inputVector = new Vector2(0, 0);
-        
+
         if (Input.GetKey(KeyCode.W)) {
             inputVector.y = +1;
         }
@@ -23,8 +24,8 @@ public class Player : MonoBehaviour
 
         inputVector = inputVector.normalized;
 
-        transform.position += (Vector3)inputVector;
-
-        Debug.Log(inputVector);
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        Debug.Log(Time.deltaTime);
     }
 }
