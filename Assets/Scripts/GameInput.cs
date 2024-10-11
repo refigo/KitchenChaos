d19824137a1b40cfe2d65a0d7bf2ad64f15c4e7b
refigo <refi@xyzcorp.io>
2024-10-11 17:06:01 +0900

    Feat: Change to the new input system

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -4,21 +4,15 @@ using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
-    public Vector2 GetMovementVectorNormalized() {
-        Vector2 inputVector = new Vector2(0, 0);
+    private PlayerInputActions playerInputActions;
 
-        if (Input.GetKey(KeyCode.W)) {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A)) {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            inputVector.x = +1;
-        }
+    private void Awake() {
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+    }
+
+    public Vector2 GetMovementVectorNormalized() {
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
 
         inputVector = inputVector.normalized;
 
