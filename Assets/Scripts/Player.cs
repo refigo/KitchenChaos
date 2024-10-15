b432a40d90c152b233248dae231ac73511ad599c
refigo <refi@xyzcorp.io>
2024-10-15 11:16:38 +0900

    Feat: Using CapsuleCast

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,8 +14,10 @@ public class Player : MonoBehaviour
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
-        float playerSize = .7f;
-        bool canMove = !Physics.Raycast(transform.position, moveDir, playerSize);
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
 
         if (canMove) {
             transform.position += moveDir * moveSpeed * Time.deltaTime;
